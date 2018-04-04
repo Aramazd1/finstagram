5a06b44b2a7f4c20af15613e06565931e0b059a6
Rafi <rafiterman@gmail.com>
2018-04-04 01:01:50 +0000

    Likes and Comments

---
+++ app/actions.rb
@@ -0,0 +1,107 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
+get '/' do
+   @posts = Post.order(created_at: :desc)
+    erb(:index)
+end
+
+get '/signup' do
+    @user = User.new
+    erb(:signup)
+end
+
+post '/signup' do
+ email      =params[:email]
+ avatar_url =params[:avatar_url]
+ username   =params[:username]
+ password   =params[:password]
+ 
+ @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+ 
+ if @user.save
+   redirect to('/login')
+ else
+     erb(:signup)
+ end
+end
+
+get '/login' do
+    erb(:login)
+end
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user
+        if user && user.password == password
+            session[:user_id] = user.id
+            redirect to('/')
+        else
+            @error_message = "Login Failed."
+            erb(:login)
+        end
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+get '/posts/new' do
+    @post = Post.new
+    erb(:"posts/new")
+end
+
+post '/posts' do
+    photo_url = params[:photo_url]
+    
+    @post = Post.new({ photo_url: photo_url, user_id: current_user.id })
+    
+    if @post.save
+        redirect(to('/'))
+    else
+       erb(:"posts/new")
+    end
+end
+
+get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb(:"posts/show")
+end
+
+get '/posts/new' do
+    @posts = post.new
+    erb(:"posts/new")
+end
+
+get 'posts' do
+    params.to_s
+end
+
+post '/comments' do
+    
+    text = params[:text]
+    post_id = params[:post_id]
+    
+    comment = Comment.new({ text: text, post_id: post_id, user_id: current_user.id })
+    
+    comment.save
+    
+    redirect(back)
+end
+
+post '/likes' do
+    post_id = params[:post_id]
+    
+    like = Like.new ({post_id: post_id, user_id: current_user.id})
+    like.save
+    
+    redirect(back)
+end
